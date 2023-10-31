63991b6ce395592021d3d03a3e6ac3354dbb2186
Kiyoshi Mikuni <houdig@gmail.com>
2023-10-31 13:39:21 +0900

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left *  0.1f);
+        }
+    }
+    void OnCollisionEnter (Collision collision)
+    {
+        Debug.Log("衝突したオブジェクト: " + collision.gameObject.name);
+    }
+}
