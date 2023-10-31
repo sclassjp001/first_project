8323c7035fdcb2d7d3388cadf24e60b712226d62
sclassjp001 <149456565+sclassjp001@users.noreply.github.com>
2023-10-31 14:35:25 +0900

    最初のバージョン

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -15,19 +15,19 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * 0.1f);
+            transform.Translate(Vector3.forward * 0.5f);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * 0.1f);
+            transform.Translate(Vector3.back * 0.5f);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * 0.1f);
+            transform.Translate(Vector3.right * 0.5f);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left *  0.1f);
+            transform.Translate(Vector3.left *  0.5f);
         }
     }
     void OnCollisionEnter (Collision collision)
